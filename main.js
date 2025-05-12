f824941160ad05174d7707822fac351bc298283d
SVK300 <104364745+SVK300@users.noreply.github.com>
2025-05-12 12:09:51 +0400

    Update main.js

--- main.js
+++ main.js
@@ -109,3 +109,5 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
